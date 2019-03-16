c4fb11c0f93e45f2c03fc776ee3484992e6a92c7
sowndharya <sownece@gmail.com>
2019-03-16 13:04:53 +0530

    adding working code

--- demo.py
+++ demo.py
@@ -1,1 +1,20 @@
-print("Hello World")
+#Remove duplicates from list or print unique elements in the list
+"""
+list1 = [1,2,3,1,2,896875,9,9,9,853,1,2,3,4]
+list2 = []
+
+for i in list1:
+  if i not in list2:
+      list2.append(i)
+
+
+print(list2)
+
+"""
+#Fetch the last value in list
+##########
+#code1
+list1 = [6,"red","blue",5,8,9]
+print(list1[-1])
+
+print(7 & 9)
